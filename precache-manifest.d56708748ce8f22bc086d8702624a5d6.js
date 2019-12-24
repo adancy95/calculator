dcf9a68714390f14c7c0418b3888eb8701b8b2a0
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-12-23 21:16:31 -0500

    Updates

---
+++ precache-manifest.d56708748ce8f22bc086d8702624a5d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e7e8cd56b7d1805f624ce6294ca82e9",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "c60d9cbad3a8341f24c8",
+    "url": "/calculator/static/css/main.3fa97900.chunk.css"
+  },
+  {
+    "revision": "bb23e757d3531116e50e",
+    "url": "/calculator/static/js/2.378ebaf3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculator/static/js/2.378ebaf3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c60d9cbad3a8341f24c8",
+    "url": "/calculator/static/js/main.a229fae9.chunk.js"
+  },
+  {
+    "revision": "c3d9a3bbcb6bc1177d26",
+    "url": "/calculator/static/js/runtime-main.250539d0.js"
+  }
+]);
